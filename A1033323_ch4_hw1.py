131f5ea3e69b8ad97ab0c28f4990220f4f77fe51
david850519 <david850519@gmail.com>
2016-03-25 15:07:21 +0800

    ch4hw

---
+++ A1033323_ch4_hw1.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+girlfriend={'Mary':'Honey','Lynn':'Baby','Jannie':'Little sweetie'}
+a=input("要給Mary請輸入0,給Lynn請輸入1,給Jannie請輸入2")
+if(a==0):
+	print girlfriend['Mary']+','
+if(a==1):
+	print girlfriend['Lynn']+','
+if(a==2):
+	print girlfriend['Jannie']+','
+print 'you are always on my mind.\nYour love makes me blind .\nWhen I first met you I knew it was a sign. '
